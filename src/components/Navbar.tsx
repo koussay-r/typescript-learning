0aca48a458200f535724fab7cc790fd0b52a3053
koussay-r <koussayr98@gmail.com>
2023-03-01 17:40:58 +0100

    small mistake fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import React,{useState} from 'react'
 import {BsPenFill} from "react-icons/bs"
 export default function Navbar() {
-    const [tasks,setTasks]=useState<string[]>([])
     const [value,setValue]=useState<string>("")
+  const [tasks,setTasks]=useState<string[]>([])
     const hanlecreateList=():void=>{
         if(value.length!==0){
             setTasks([...tasks,value])
@@ -17,7 +17,7 @@ export default function Navbar() {
   return (
     <div className='flex pt-3 px-5 border border-transparent border-b-gray-500 shadow pb-3 justify-between'>
         <img src={require("./../assets/logo.png")} alt="" className='w-[50px]'/>
-        <input value={tasks} onChange={handleValue}type={"text"} placeholder="Write a task" className=" bg-slate-100 pl-3 border border-blue-500 rounded-md w-[50%]" />
+        <input value={value} onChange={handleValue} type={"text"} placeholder="Write a task" className=" bg-slate-100 pl-3 border border-blue-500 rounded-md w-[50%]" />
         <BsPenFill size={35} onClick={hanlecreateList} className=" mt-1 cursor-pointer text-blue-600"/>
     </div>
   )
